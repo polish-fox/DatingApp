9f3d634863c2bcf35ee2ad1dc084fe0dafba09b2
Polish-fox <ernestlis@o2.pl>
2021-11-03 14:09:54 +0100

    End section 4

---
+++ API/Data/Migrations/20211103094829_UserPasswordAdde.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class UserPasswordAdde : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "Users",
+                newName: "PasswordHash");
+
+            migrationBuilder.RenameColumn(
+                name: "PasswordSalt",
+                table: "Users",
+                newName: "PasswordSalt");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "Users",
+                newName: "PasswordHash");
+
+            migrationBuilder.RenameColumn(
+                name: "PasswordSalt",
+                table: "Users",
+                newName: "PasswordSalt");
+        }
+    }
+}
